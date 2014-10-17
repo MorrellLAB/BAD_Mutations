3df365591036ed1652812356e7d75b3491b15634
Yancy Ribbens Laptop <yancy.ribbens@gmai.com>
2014-10-17 11:52:37 -0500

    add arg parse

--- fetch_cds.py
+++ fetch_cds.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
 import requests
+import argparse
 from xml.etree import ElementTree
 from requests.auth import HTTPDigestAuth
+
+def parse_args():
+    print "parse args"
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--foo', help='foo help')
+    args = parser.parse_args()
+    parser.print_help()
 
 def sign_on():
     login_data = {'commit':'1','login':'ribb0013','password':''}
@@ -28,9 +36,11 @@ def download_file(url,s):
     return local_filename
 
 def main():
+    parse_args()
     test_fil_url ='http://genome.jgi.doe.gov/PhytozomeV10/download/_JAMO/53112a6949607a1be0055904/Sitalica_164_v2.1.cds_primaryTranscriptOnly.fa.gz'
 
     s = sign_on()
     download_file(test_fil_url,s)
 
-main()
+parse_args()
+#main()
