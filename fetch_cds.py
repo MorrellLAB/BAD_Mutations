b1c413107abb42f57d8069b87f9477ade90bc4de
Yancy Ribbens Laptop <yancy.ribbens@gmai.com>
2014-10-17 11:23:33 -0500

    add site login and create session

--- fetch_cds.py
+++ fetch_cds.py
@@ -2,18 +2,35 @@
 
 import requests
 from xml.etree import ElementTree
+from requests.auth import HTTPDigestAuth
 
-test_fil_url ='http://genome.jgi.doe.gov/PhytozomeV10/download/_JAMO/53112a6949607a1be0055904/Sitalica_164_v2.1.cds_primaryTranscriptOnly.fa.gz'
+def sign_on():
+    login_data = {'commit':'1','login':'ribb0013','password':''}
+    s = requests.Session()
+    s.auth = ('user', 'pass')
+    s.headers.update({'x-test': 'true'})
 
-def download_file(url):
+    signon_page ="https://signon.jgi.doe.gov/signon"
+    requests.post(signon_page,login_data,verify=True)
+    return s
+
+
+def download_file(url,s):
     local_filename = url.split('/')[-1]
-    # NOTE the stream=True parameter
-    r = requests.get(url, stream=True)
+    r = s.get(url, stream=True)
+    print r.headers
+    print r.request.headers
     with open(local_filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024): 
-            if chunk: # filter out keep-alive new chunks
+            if chunk:
                 f.write(chunk)
                 f.flush()
     return local_filename
 
-download_file(test_fil_url)
+def main():
+    test_fil_url ='http://genome.jgi.doe.gov/PhytozomeV10/download/_JAMO/53112a6949607a1be0055904/Sitalica_164_v2.1.cds_primaryTranscriptOnly.fa.gz'
+
+    s = sign_on()
+    download_file(test_fil_url,s)
+
+main()
