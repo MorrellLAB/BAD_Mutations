ff4cc61c562b818b24e28c3b07f8be8fb9afb42b
Yancy Ribbens Laptop <yancy.ribbens@gmai.com>
2014-10-21 16:29:52 -0500

    fix signon

--- fetch_cds.py
+++ fetch_cds.py
@@ -13,20 +13,16 @@ def parse_args():
     return args
 
 def sign_on(args):
-    login_data = {'commit':'1','login':args.user,'password':args.password}
-    s = requests.Session()
-    s.auth = ('user', 'pass')
-    s.headers.update({'x-test': 'true'})
-
-    signon_page ="https://signon.jgi.doe.gov/signon"
-    requests.post(signon_page,login_data,verify=True)
+    s = requests.session()
+    payload = {'login':args.user,'password':args.password}
+    signon_page ="https://signon.jgi.doe.gov/signon/create"
+    r = s.post(signon_page, data=payload)
+    print r.text
     return s
 
 def download_file(url,s):
     local_filename = url.split('/')[-1]
     r = s.get(url, stream=True)
-    print r.headers
-    print r.request.headers
     with open(local_filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024): 
             if chunk:
@@ -34,11 +30,12 @@ def download_file(url,s):
                 f.flush()
     return local_filename
 
+
 def main():
     args = parse_args()
-    test_fil_url ='http://genome.jgi.doe.gov/PhytozomeV10/download/_JAMO/53112a6949607a1be0055904/Sitalica_164_v2.1.cds_primaryTranscriptOnly.fa.gz'
-
+    test_file_url ='http://genome.jgi.doe.gov/PhytozomeV10/download/_JAMO/53112a6949607a1be0055904/Sitalica_164_v2.1.cds_primaryTranscriptOnly.fa.gz'
     s = sign_on(args)
-    download_file(test_fil_url,s)
+      
+    download_file(test_file_url,s)
 
 main()
