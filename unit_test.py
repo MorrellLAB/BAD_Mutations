872cc188d4375d842f4934021951bd172e5cdfda
Yancy Ribbens Laptop <yancy.ribbens@gmai.com>
2014-10-21 17:10:28 -0500

    add unit test file

---
+++ unit_test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import fetch_cds
+import unittest
+
---
+++ unittest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import fetch_cds
+import unittest
+
+class TestCDSFetch(unittest.TestCase):
+
+    def setUp(self):
+
+    def test_has_xml(self):
+        print "test xml here"
+
+if __name__ == '__main__':
+    unittest.main()
