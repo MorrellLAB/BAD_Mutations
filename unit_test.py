41a1dc0f16a1182894d2937d7d382f09ca0e3a4c
Yancy Ribbens Laptop <yancy.ribbens@gmai.com>
2014-10-21 21:30:50 -0500

    add jgi create to setup

--- unit_test.py
+++ unit_test.py
@@ -8,16 +8,21 @@ class TestJGIFunctions(unittest.TestCase):
     def setUp(self):
         self.jgi_user = os.environ['jgi_user']
         self.jgi_password = os.environ['jgi_password']
+        self.jgi = genome_jgi.JGIUtils(self.jgi_user,self.jgi_password)
 
     def test_user(self):
         self.assertTrue( self.jgi_user != None)
         self.assertTrue( self.jgi_password != None)
 
     def test_fetch_xml(self):
-        jgi = genome_jgi.JGIUtils(self.jgi_user,self.jgi_password)
-        xml = jgi.fetch_xml()
+        xml = self.jgi.fetch_xml()
         self.assertTrue( xml != None )
         self.assertTrue( xml.find("cds") )
+
+    #def test_cds_list(self):
+        
+        
+
 
 if __name__ == '__main__':
       unittest.main()
