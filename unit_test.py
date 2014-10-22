53d9d54bf4d98ccfe9a647e75345272a8de0f909
Yancy Ribbens Laptop <yancy.ribbens@gmai.com>
2014-10-21 19:52:10 -0500

    update unit test files

--- unit_test.py
+++ unit_test.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
-import fetch_cds
+import genome_jgi
 import unittest
 
+class TestJGIFunctions(unittest.TestCase):
+
+    def setUp(self):
+        print "run tests"
+
+    def test_fetch_xml(self):
+        print "fetch xml"
+
+if __name__ == '__main__':
+      unittest.main()
