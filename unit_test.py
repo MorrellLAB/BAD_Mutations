5cbca8517670b4c4e5444bf7d265c69b83d2cc66
Yancy Ribbens Laptop <yancy.ribbens@gmai.com>
2014-10-21 20:09:12 -0500

    add cred as env variables for testing

--- unit_test.py
+++ unit_test.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 import genome_jgi
 import unittest
+import os
 
 class TestJGIFunctions(unittest.TestCase):
 
     def setUp(self):
-        print "run tests"
+        self.jgi_user = os.environ['jgi_user']
+        self.jgi_password = os.environ['jgi_password']
+
+    def test_user(self):
+        self.assertTrue( self.jgi_user != None)
+        self.assertTrue( self.jgi_password != None)
 
     def test_fetch_xml(self):
-        print "fetch xml"
+        print "jgi_user"
 
 if __name__ == '__main__':
       unittest.main()
