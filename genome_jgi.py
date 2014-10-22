b0b867745578b06e4df0b0e9c08f63d36ecf0bce
Yancy Ribbens Laptop <yancy.ribbens@gmai.com>
2014-10-21 21:22:15 -0500

    make JGIUtils class

--- genome_jgi.py
+++ genome_jgi.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python
 import requests
 
-def sign_on(user,password):
-    s = requests.session()
-    payload = {'login':user,'password':password}
-    signon_page ="https://signon.jgi.doe.gov/signon/create"
-    r = s.post(signon_page, data=payload)
-    return s
+class JGIUtils(object):
 
-def download_file(url,s):
-    local_filename = url.split('/')[-1]
-    r = s.get(url, stream=True)
-    with open(local_filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024): 
-            if chunk:
-                f.write(chunk)
-                f.flush()
-    return local_filename
+    def __init__(self,user,password):
+        self.s = self.sign_on(user,password)
 
-def fetch_xml(s):
-    url = "http://genome.jgi.doe.gov/ext-api/downloads/get-directory"
-    payload = {'organism':'PhytozomeV10'}
-    xml = s.get(url,params=payload)
-    return xml.text
+    def sign_on(self,user,password):
+        s = requests.session()
+        payload = {'login':user,'password':password}
+        signon_page ="https://signon.jgi.doe.gov/signon/create"
+        r = s.post(signon_page, data=payload)
+        return s
+
+    def download_file(self,url,s):
+        local_filename = url.split('/')[-1]
+        r = s.get(url, stream=True)
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024): 
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+        return local_filename
+
+    def fetch_xml(self,s):
+        url = "http://genome.jgi.doe.gov/ext-api/downloads/get-directory"
+        payload = {'organism':'PhytozomeV10'}
+        xml = s.get(url,params=payload)
+        return xml.text
+
+#def fetch_cds_list():
+    
 
 #def main():
 #    args = parse_args()
