e61c68ff0560b6e64477441d9d97e66cfd31d558
Tom Kono <konox006@umn.edu>
2015-03-03 17:33:19 -0600

    Remove old files
    Part of rewrite

--- Species_Databases.py
+++
@@ -1,8 +0,0 @@
-#   This file is just meant to be imported by BLAST_Query.py
-#   it contains a list that holds the paths to species-specific databases
-
-species = [ '',
-            '',
-            '',
-            '',
-        ]
