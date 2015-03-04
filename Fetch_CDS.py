8eb76a9d71e6dcea84f457c75b8b031c58b91bb6
Tom Kono <konox006@umn.edu>
2015-03-04 13:51:02 -0600

    Finalize CDS fetching piece
    Remove old code, add function to download CDS.

--- Fetch_CDS.py
+++ Fetch_CDS.py
@@ -2,9 +2,24 @@
 #   This script will take the URLs of the CDS files and download them from
 #   Phytozome.
 
-#   To handle HTTP requests
-import requests
+#   Define a base URL for downloading
+dl_base = 'http://genome.jgi.doe.gov'
 
-#   A function to download the CDS file
-def download_cds(cds_url):
-    pass
+#   A function that downloads the CDS file
+def dl(session, url):
+    #   The local filename should be the basename of the remote file
+    localname = url.split('/')[-1]
+    #   With stream=True, it downloads the response right away
+    r = session.get(dl_base + url, stream=True)
+    #   Save the file
+    with open(localname, 'wb') as f:
+        #   Take the file in pieces
+        for chunk in r.iter_content(chunk_size=1024):
+            #   Empty chunks are for keepalive purposes, we don't save those
+            if chunk:
+                #   Write the file to disk
+                f.write(chunk)
+                #   and flush the buffer
+                f.flush()
+    return localname
+
--- Phytozome.py
+++ Phytozome.py
@@ -12,72 +12,15 @@ import Get_URLs as GU
 #   Import the downloading script
 import Fetch_CDS as FC
 
-arguments = PA.parse_args()
+#   Main function
+def main():
+    arguments = PA.parse_args()
+    session = GU.signon(arguments.user, arguments.password)
+    urls = GU.extract_all_urls(session)
+    cds = GU.extract_cds_urls(urls)
+    for c in cds:
+        fname = FC.dl(session, c)
+        print 'Downloaded ' + fname
 
-session = GU.signon(arguments.user, arguments.password)
-urls = GU.extract_all_urls(session)
-cds = GU.extract_cds_urls(urls)
-print cds
-# import requests
-# from xml.etree import ElementTree
-
-# class JGIUtils(object):
-
-#     def __init__(self,user,password):
-#         self.s = self.sign_on(user,password)
-
-#     def sign_on(self,user,password):
-#         s = requests.session()
-#         payload = {'login':user,'password':password}
-#         signon_page ="https://signon.jgi.doe.gov/signon/create"
-#         r = s.post(signon_page, data=payload)
-#         return s
-
-#     def download_file(self,url,s):
-#         local_filename = url.split('/')[-1]
-#         r = s.get(url, stream=True)
-#         with open(local_filename, 'wb') as f:
-#             for chunk in r.iter_content(chunk_size=1024): 
-#                 if chunk:
-#                     f.write(chunk)
-#                     f.flush()
-#         return local_filename
-
-#     def fetch_xml(self,url):
-#         payload = {'organism':'PhytozomeV10'}
-#         xml = self.s.get(url,params=payload)
-#         return xml.content
-
-#     def fetch_url_list(self,xml):
-#         url_list = []
-#         tree = ElementTree.fromstring(xml)
-#         for elem in tree.findall('.//file'):
-#             url = elem.attrib.get('url')
-#             url_list.append(url)
-#         return url_list
-
-#     def fetch_cds_list(self,all_urls):
-#         cds_url_list = []
-#         for url in all_urls:
-#             part = url.split(".")
-#             for p in part:
-#                 if p == "cds":
-#                     cds_url_list.append( url.split("/")[-1] )
-#         return cds_url_list
-
-#     def download_cds_files(self):
-#         xml_url = "http://genome.jgi.doe.gov/ext-api/downloads/get-directory"
-#         xml = self.fetch_xml(xml_url)
-#         url_list = self.fetch_url_list(xml)
-#         cds_list = self.fetch_cds_list(url_list)
-#         for cds_file in cds_list:
-#             dl_url = "http://genome.jgi.doe.gov/PhytozomeV10/download/_JAMO/53ebd93a0d878557fd3b5efd/" + cds_file
-#             print "downloding: " + dl_url
-#             self.download_file(dl_url,self.s)
-
-# #def main():
-# #    args = parse_args()
-# #    s = sign_on(args)
-# #    fetch_xml(s)
-
-# #main()
+#   Do the work here
+main()
