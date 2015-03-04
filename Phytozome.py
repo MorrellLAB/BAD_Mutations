a2bef10fb662c4b3508c430aaf4fb5f4d76e7bf2
Tom Kono <konox006@umn.edu>
2015-03-04 13:51:59 -0600

    Add some comments

--- Phytozome.py
+++ Phytozome.py
@@ -14,9 +14,13 @@ import Fetch_CDS as FC
 
 #   Main function
 def main():
+    #   Parse the arguments
     arguments = PA.parse_args()
+    #   Start a new login session to Phyotozome
     session = GU.signon(arguments.user, arguments.password)
+    #   Get the list of all URLs from the downloads tree
     urls = GU.extract_all_urls(session)
+    #   Get the CDS only
     cds = GU.extract_cds_urls(urls)
     for c in cds:
         fname = FC.dl(session, c)
