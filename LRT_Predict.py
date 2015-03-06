d025c8841a55190eaa0b037be6a730659f490820
Tom Kono <konox006@umn.edu>
2015-03-05 21:37:25 -0600

    Fix MD5 checks. Should work now

--- LRT_Predict.py
+++ LRT_Predict.py
@@ -54,10 +54,11 @@ def fetch(base, user, password):
     #   Get the list of all URLs and md5s from the downloads tree
     urls, md5s = get_urls.extract_all_urls(session)
     #   Get the CDS only
-    cds, md5s = get_urls.extract_cds_urls(urls, md5s)
+    cds, md5s_flt = get_urls.extract_cds_urls(urls, md5s)
+    print len(cds), len(md5s_flt)
     #   What are the filenames?
     cds_names = []
-    for c, m in zip(cds, md5s):
+    for c, m in zip(cds, md5s_flt):
         #   What is the local name?
         local_name = fetch_cds.local_name(c)
         #   And the species name?
@@ -69,15 +70,10 @@ def fetch(base, user, password):
         os.chdir(spdir)
         #   Do these files exist already?
         if file_checks.file_exists(local_name):
-            #   If so, check for the md5 file
-            if file_checks.file_exists(local_name + '.md5'):
-                #   If it's there, compare the two MD5s
-                md5s_same = file_checks.md5_is_same(local_name + '.md5', m)
-            else:
-                #   If it's not there, calculate it
-                calc_md5 = file_checks.calculate_md5(local_name)
-                #   And then compare
-                md5s_same = file_checks.md5_is_same(calc_md5, m)
+            #   If it's not there, calculate it
+            calc_md5 = file_checks.calculate_md5(local_name)
+            #   And then compare
+            md5s_same = file_checks.md5_is_same(calc_md5, m)
             #   Then we check if the md5s are the same. If they are the same,
             #   then we skip the file, and move on. Else, we download a new one
             if md5s_same:
@@ -91,12 +87,7 @@ def fetch(base, user, password):
                     fetch_cds.dl(session, c, local_name)
                     r_md5 = m
                     l_md5 = file_checks.calculate_md5(local_name)
-                    print r_md5, l_md5
                     same = file_checks.md5_is_same(l_md5, r_md5)
-                #   Save the md5 for later
-                handle = open(local_name + '.md5', 'w')
-                handle.write(l_md5 + '\n')
-                handle.close()
         #   If the files don't exist already, then we just download them
         else:
             #   a variable to tell if we've checked out
@@ -107,10 +98,6 @@ def fetch(base, user, password):
                 r_md5 = m
                 l_md5 = file_checks.calculate_md5(local_name)
                 same = file_checks.md5_is_same(l_md5, r_md5)
-            #   Save the md5 for later
-            handle = open(local_name + '.md5', 'w')
-            handle.write(l_md5 + '\n')
-            handle.close()
     print "Done!"
     return
 
