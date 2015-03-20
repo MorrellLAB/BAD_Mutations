4f5728db02c9da001bf869acb12e945bc83738e9
Tom Kono <konox006@umn.edu>
2015-03-20 13:18:13 -0500

    Remove unnecessary diagnostic messages

--- lrt_predict/Blast/blast_search.py
+++ lrt_predict/Blast/blast_search.py
@@ -136,7 +136,7 @@ class BlastSearch:
             self.mainlog.debug('Using ' + blastdbcmd_path)
             for database, seqID in self.homologues.iteritems():
                 s_out, s_err = sequence_fetch.blastdbcmd(blastdbcmd_path, database, seqID)
-                self.mainlog.debug(str(s_out) + str(s_err))
+                temp_output.write(s_out.strip())
         else:
             self.mainlog.debug('Using regex')
             for database, seqID in self.homologues.iteritems():
