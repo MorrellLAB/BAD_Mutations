0149d96fe68d645bc66aa71a8e5910e7b4b6507c
Tom Kono <konox006@umn.edu>
2015-04-27 13:49:15 -0500

    Clean up main file for readability and style

--- LRT_Predict.py
+++ LRT_Predict.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-
-#   Script to perform LRT from Chun and Fay (2009) to predict deleterious SNPs
-#   in plants.
-#   Requires a user name and password for the JGI phyotzome portal (Free)
-#   Thomas Kono
-#       March 4, 2015
-#       Saint Paul, MN
-
+"""
+Script to perform LRT from Chun and Fay (2009) to predict deleterious SNPs in
+plants. Requires a user name and password for the JGI phyotzome portal (Free)
+Author: Thomas Kono
+        March 4, 2015
+        Saint Paul, MN
+"""
 #   Dependencies:
 #       1) requests http://www.python-requests.org
 #       2) argparse https://code.google.com/p/argparse/
@@ -14,15 +13,8 @@
 #       3) Biopython http://biopython.org/
 #       4) BLAST+ executables from NCBI
 
-
-#   To handle passwords
-import getpass
-#   To cd
-import os
 #   to check arguments
 import sys
-#   for verbosity messages
-import logging
 
 #   Import the dependency checking script
 from lrt_predict.General import check_modules
@@ -32,8 +24,8 @@ from lrt_predict.General import set_verbosity
 from lrt_predict.General import parse_args
 
 
-#   Define a function to set up the environment
-def setup(arg, log):
+def setup(arg):
+    """A function to write the configuration file."""
     #   Check for the required modules for setup
     setupdeps = check_modules.check_modules(setup=True)
     if setupdeps:
@@ -42,26 +34,31 @@ def setup(arg, log):
     #   Import the setup script
     import lrt_predict.Setup.setup_env as setup_env
     #   Start a new instance of the configuration class
-    s = setup_env.SetupEnv(arg['loglevel'])
+    s_env = setup_env.SetupEnv(arg['loglevel'])
     #   Then create the new configuration
-    s.set_user_vars(arg['base'], arg['target'], arg['evalue'],
-                 arg['codon'], arg['missing_threshold'], arg['config'])
-    s.get_exe_paths()
-    s.write_config()
+    s_env.set_user_vars(arg['base'], arg['target'], arg['evalue'],
+                        arg['codon'], arg['missing_threshold'], arg['config'])
+    s_env.get_exe_paths()
+    s_env.write_config()
     return
 
 
-#   Define a function for fetching
 def fetch(arg, log):
+    """A function to download the appropriate files from Ensembl Plants and
+    Phytozome. Will also convert them to BLAST databases."""
     fetchdeps = check_modules.check_modules(fetch=True)
     if fetchdeps:
         check_modules.missing_mods(fetchdeps)
         exit(1)
-    #   Next we check for the presence of the utilities we need to 
+    #   Next we check for the presence of the utilities we need to
     #   do the fetching
-    missing_reqs = check_modules.missing_executables(['bash', 'makeblastdb', 'gzip', 'sum'])
+    missing_reqs = check_modules.missing_executables(['bash',
+                                                      'makeblastdb',
+                                                      'gzip',
+                                                      'sum'])
     if missing_reqs:
-        log.error('Some required executables were not found on your system: ' + '\n'.join(missing_reqs) + '\nPlease install them to continue.')
+        log.error('Some required executables were not found on your system: ' +
+                  '\n'.join(missing_reqs)+'\nPlease install them to continue.')
         exit(1)
     #   Import the main fetching script
     #   We do it here, since we only want to import this if we are fetching
@@ -69,37 +66,39 @@ def fetch(arg, log):
     import lrt_predict.Fetch.ensembl as ensembl
     #   Create a new Phytozome instance that will handle our work with
     #   the JGI Genomes Portal.
-    log.info('Creating a new Phytozome instance to fetch data.')
-    #   We give it username, password, base directory, whether or not we have to log in and 
-    p = phytozome.Phytozome(arg['user'], arg['password'], arg['base'], arg['convert_only'], arg['loglevel'])
-    log.info('Creating a new Ensembl instance to fetch data.')
-    ens = ensembl.EnsemblPlants(arg['base'], arg['convert_only'], arg['loglevel'])
+    log.debug('Creating a new Phytozome instance to fetch data.')
+    phy = phytozome.Phytozome(arg['user'], arg['password'], arg['base'],
+                              arg['convert_only'], arg['loglevel'])
+    log.debug('Creating a new Ensembl instance to fetch data.')
+    ens = ensembl.EnsemblPlants(arg['base'], arg['convert_only'],
+                                arg['loglevel'])
     if arg['convert_only']:
-        log.info('Only converting files.')
+        log.debug('Only converting files.')
         ens.convert()
-        p.convert()
+        phy.convert()
     elif arg['fetch_only']:
-        log.info('Only downloading files.')
+        log.debug('Only downloading files.')
         log.info('Fetching from Ensembl Plants...')
         ens.get_ftp_urls()
         ens.download_files()
         log.info('Fetching from Phytozome...')
-        p.get_xml_urls()
-        p.fetch_cds()
+        phy.get_xml_urls()
+        phy.fetch_cds()
     else:
-        log.info('Downloading and converting Ensembl Plants files...')
+        log.debug('Downloading and converting Ensembl Plants files...')
         ens.get_ftp_urls()
         ens.download_files()
-        log.info('Downloading and converting files.')
-        p.get_xml_urls()
-        p.fetch_cds()
+        log.debug('Downloading and converting files.')
+        phy.get_xml_urls()
+        phy.fetch_cds()
         ens.convert()
-        p.convert()
+        phy.convert()
     return
 
 
-#   Define a function for BLASTing
 def blast(arg, log):
+    """A function to search the databses with BLAST and collect the
+    homologous sequences from them."""
     blastdeps = check_modules.check_modules(predict=True)
     #   Check the module dependencies for the BLAST function
     if blastdeps:
@@ -108,60 +107,69 @@ def blast(arg, log):
     missing_reqs = check_modules.missing_executables(['bash', 'tblastx'])
     #   And then check the executable dependencies
     if missing_reqs:
-        log.error('Some required executables were not found on your system: ' + '\n'.join(missing_reqs) + '\nPlease install them to continue.')
+        log.error('Some required executables were not found on your system: ' +
+                  '\n'.join(missing_reqs)+'\nPlease install them to continue.')
         exit(1)
     #   If all that checks out, import the BLAST class script
     from lrt_predict.Blast import blast_search
     log.info('Creating a new instance to BLAST.')
-    b = blast_search.BlastSearch(arg['base'], arg['fasta'], arg['evalue'], arg['loglevel'])
-    b.blast_all()
+    b_search = blast_search.BlastSearch(arg['base'], arg['fasta'],
+                                        arg['evalue'], arg['loglevel'])
+    b_search.blast_all()
     #   hom contains the file object that has the unaligned sequence in it.
-    hom = b.get_hit_seqs()
+    hom = b_search.get_hit_seqs()
     return hom
 
 
-#   Define a function for aligning
 def align(arg, unaligned, log):
+    """A function to align the homologous sequences with prank-msa, and return
+    the aligned sequences and the phylogenetic tree."""
     aligndeps = check_modules.check_modules(predict=True)
     if aligndeps:
-        check_modules.mossing_mods(aligndeps)
+        check_modules.missing_mods(aligndeps)
         exit(1)
     #   Check for the required executables
     missing_reqs = check_modules.missing_executables(['bash', 'prank'])
     if missing_reqs:
-        log.error('Some required executables were not found on your system: ' + '\n'.join(missing_reqs) + '\nPlease install them to continue.')
+        log.error('Some required executables were not found on your system: ' +
+                  '\n'.join(missing_reqs)+'\nPlease install them to continue.')
         exit(1)
     #   Then we import the necessary modules
-    from lrt_predict.Predict import align
+    from lrt_predict.Predict import align as aligner
     log.info('Creating a new instance of PrankAlign.')
-    a = align.PrankAlign(unaligned, arg['fasta'], arg['codon'], arg['loglevel'])
+    aln = aligner.PrankAlign(unaligned, arg['fasta'], arg['codon'],
+                             arg['loglevel'])
     #   Then align them
-    stdout, stderr, outfile = a.prank_align()
+    stdout, stderr, outfile = aln.prank_align()
     log.debug('stdout: \n' + stdout)
     log.debug('stderr: \n' + stderr)
     return outfile
 
 
-#   Define a function for predicting
 def predict(arg, nuc, tree, log):
+    """A function to run the HYPHY codon prediction model on each column of
+    the alignment and return a score for each one."""
     predictdeps = check_modules.check_modules(predict=True)
     if predictdeps:
-        check_modules.mossing_mods(predictdeps)
+        check_modules.missing_mods(predictdeps)
         exit(1)
     #   Check for the required executables
     missing_reqs = check_modules.missing_executables(['bash', 'HYPHYSP'])
     if missing_reqs:
-        log.error('Some required executables were not found on your system: ' + '\n'.join(missing_reqs) + '\nPlease install them to continue.')
+        log.error('Some required executables were not found on your system: ' +
+                  '\n'.join(missing_reqs)+'\nPlease install them to continue.')
         exit(1)
     #   import the predict script
-    from lrt_predict.Predict import predict
+    from lrt_predict.Predict import predict as predictor
     #   Create a new instance of class LRTPredict
-    lrt = predict.LRTPredict(nuc, tree, arg['fasta'], arg['substitutions'], arg['loglevel'])
+    lrt = predictor.LRTPredict(nuc, tree, arg['fasta'], arg['substitutions'],
+                               arg['loglevel'])
     position = lrt.get_query_position()
     return position
 
-#   Main function
+
 def main():
+    """The main function."""
     #   The very first thing we do is do a base check to make sure that we can
     #   parse arguments
     dep = check_modules.check_modules()
@@ -186,12 +194,13 @@ def main():
         #   If the user wants to setup, then don't bother trying to validate
         #   the config.
         if arguments['action'] != 'setup':
-            cfg = config.ConfigHandler(arguments['config'], arguments, arguments['loglevel'])
+            cfg = config.ConfigHandler(arguments['config'], arguments,
+                                       arguments['loglevel'])
             if cfg.is_valid():
                 cfg.read_vars()
                 config_opts = cfg.merge_options()
             else:
-                log.error('Config file is not valid!')
+                loglevel.error('Config file is not valid!')
                 exit(1)
         #   Else, just set it to the options that were passed
         else:
@@ -204,7 +213,7 @@ def main():
         loglevel.debug(arguments_valid['action'] + ' subcommand was invoked')
         #   Which command was invoked?
         if arguments_valid['action'] == 'setup':
-            setup(arguments_valid, loglevel)
+            setup(arguments_valid)
         elif arguments_valid['action'] == 'fetch':
             fetch(arguments_valid, loglevel)
         elif arguments_valid['action'] == 'predict':
