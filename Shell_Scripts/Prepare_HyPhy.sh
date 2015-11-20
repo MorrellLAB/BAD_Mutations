bab92e305aa01423aa1e7110ae1928a0f0e20ec8
TomJKono <kono.yoshitaka@gmail.com>
2015-11-20 14:47:24 -0600

    Add script to sanitize HyPhy inputs

---
+++ Shell_Scripts/Prepare_HyPhy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#   Script to remove spaces, bootstrap numbers, and other "unfriendly"
+#   characters from the input phylogenetic tree and multiple sequence
+#   alignment for HyPhy.
+#   From Justin C. Fay
+
+set -e
+set -u
+set -o pipefail
+
+FASTA="$1"
+TREE="$2"
+
+#   In place, remove the ' <unknown description>' tag from the alignment
+sed -i 's/[[:space:]]<unknown description>//g' ${FASTA}
+
+#   In place, remove the same from the tree, as well as bootstrap values
+sed -i 's/[[:space:]]<unknown description>//g' ${TREE}
+#   '-r' (Extended regex) is a GNU option
+if [ "$(uname)" == "Linux" ]
+    then
+        sed -i -r 's/\\\)[0-9].[0-9]+/\\\)/g' ${TREE}
+        sed -i -r "s/'//g" ${TREE}
+#   The equivalent is -E in MacOS
+elif [ "$(uname)" == "Darwin" ]
+    then
+        sed -i -E 's/\\\)[0-9].[0-9]+/\\\)/g' ${TREE}
+        sed -i -E "s/'//g" ${TREE}
+fi
