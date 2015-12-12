a0325d50c1787a1284698523aab3e42b9b679a61
Paul Hoffman <hoff0792@umn.edu>
2015-12-12 17:05:54 -0600

    Changed downloading of BioPython and Requests to use Pip. Added download routine for Pip and Pasta

--- Shell_Scripts/get_dependencies.sh
+++ Shell_Scripts/get_dependencies.sh
@@ -15,21 +15,33 @@ set -o pipefail
 #	Set our variables from input
 #	The first variable is the dependencies diretory, and everything else
 #	are the dependencies that need to be downloaded
-DEPSDIR=$1
-MISSING=${@:1}
+DEPSDIR="$1"
+MISSING="${@:1}"
 
 #	Make the dependencies directory and cd into it
-mkdir -p $DEPSDIR
-cd $DEPSDIR
+mkdir -p "${DEPSDIR}"
+cd "${DEPSDIR}"
 
-for x in $MISSING
+#   A function to check for, download, and isntall Pip
+function getPip() {
+    if ! `command -v pip > /dev/null 2> /dev/null`
+    then
+        wget https://bootstrap.pypa.io/get-pip.py
+        python get-pip.py --user
+        echo export PATH='$PATH':"${HOME}"/.local/bin >> "${HOME}"/.bash_profile
+        source "${HOME}"/.bash_profile
+        rm get-pip.py
+    fi
+}
+
+for x in "${MISSING}"
 do
-	case $x in
+	case "${x}" in
 		"tBLASTx" )
-			cd $DEPSDIR
+			cd "${DEPSDIR}"
 			#	Install BLAST
 				#	Pull BLAST from NCBI with wget
-				#	Designed to always pull latest version 
+				#	Designed to always pull latest version
 				#	unless NCBI changes their ftp heirarchy
 			wget --no-directories --progress=bar -r -A.tar.gz ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/
 				#	Get rid of all OS-specifc installers
@@ -43,18 +55,18 @@ do
 			./configure
 			make
 				#	Cleanup tarball
-			cd $DEPSDIR
+			cd "${DEPSDIR}"
 			rm ncbi*tar.gz
 			;;
 		"PRANK" )
-			cd $DEPSDIR
+			cd "${DEPSDIR}"
 			#	Install PRANK
 				#	Pull PRANK from Wasabi
 				#	Designed to always pull latest version
 				#	unless Wasabi changes their downloads page
 			wget --no-directories --progress=bar -r -np -l 1 -A.tgz http://wasabiapp.org/download/prank
 				#	Get rid of all OS-specific installers
-				find . -maxdepth 1 -not -name "prank.source*" -name "*.tgz" -delete
+			find . -maxdepth 1 -not -name "prank.source*" -name "*.tgz" -delete
 				#	Extract the source file
 				#	Designed to always install the lastest version of PRANK
 				#	unless Wasabi changes their file naming scheme
@@ -63,28 +75,41 @@ do
 			cd prank-msa/src
 			make
 				#	Cleanup tarball and robots.txt
-			cd $DEPSDIR
+			cd "${DEPSDIR}"
 			rm prank*.tgz robots.txt
 			;;
-		"requests" )
-			cd $DEPSDIR
-			#	Install Requests
-				#	Pull Requests from GitHub
-				#	Requires Git to be installed
+        "pasta" )
+            #   Install Pasta
+                #   Pull Pasta from GitHub
+                #	Requires Git to be installed
 				#	Does not require a GitHub account
-			git clone git://github.com/kennethreitz/requests.git
-				#	Install Requests
-			cd requests
-			REQ=`pwd`
-			mkdir $REQ/modules
-			MOD=$REQ/modules
-			export PYTHONPATH="$PYTHONPATH:$REQ:$MOD"
-			python setup.py build
-			python setup.py install --install-base="." --install-lib='$base/modules' --install-scripts='$base/bin' --install-data='$base/data'/ --install-headers='$base/include/'
-			cd $DEPSDIR
+            git clone https://github.com/smirarab/pasta.git
+                #   Pull sate-tools from GitHub
+                #	Requires Git to be installed
+				#	Does not require a GitHub account
+            if [[ $( uname ) -eq "Linux" ]] # If we are on Linux
+            then # Get the linux version of sate-tools
+                git clone https://github.com/smirarab/sate-tools-linux.git
+            elif [[ $( uname ) -eq "Darwin" ]] # If we're on Mac OS X
+            then # Get the Mac OS X version of sate-tools
+                https://github.com/smirarab/sate-tools-mac.git sate-tools
+            else # If we aren't on either
+                echo "Please use Mac OS X or Linux"
+                exit 1
+            fi
+            cd pasta
+            python setup.py develop --user
+            cd "${DEPSDIR}"
+		"requests" )
+			cd "${DEPSDIR}"
+			#	Install Requests
+				#	Check for Pip
+				#	Install Requests using Pip
+			getPip
+            pip install --user requests
 			;;
 		"HyPhy" )
-			cd $DEPSDIR
+			cd "${DEPSDIR}"
 			#	Install HyPhy
 				#	Pull HyPhy from GitHub
 				#	Requires Git to be installed
@@ -94,29 +119,15 @@ do
 			cd hyphy
 			cmake -DINSTALL_PREFIX=./ ./
 			make install
-			cd $DEPSDIR
+			cd "${DEPSDIR}"
 			;;
 		"Bio" )
-			cd $DEPSDIR
+			cd "${DEPSDIR}"
 			#	Install BioPython
-				#	Pull BioPython with wget
-				#	Designed to always pull latest version 
-				#	unless BioPython changes their downloads page
-			wget -O - http://biopython.org/DIST > biopython.txt
-			BIO=`grep -E -o 'biopython-[0-9]\.[0-9a-b\.]*tar\.gz' biopython.txt | tail -1`
-			wget --no-directories --progress=bar --timestamping -r http://biopython.org/DIST/$BIO
-				#	Extract the source file
-				#	Designed to always install the latest version of BioPython
-				#	unless BioPython changes their file naming scheme
-			tar -xvzf $BIO
-				#	Install BioPython
-			cd biopython*
-			python setup.py build
-			python setup.py test
-			python setup.py install --home=./
-				#	Cleanup tarball and biopython.txt
-			cd $DEPSDIR
-			rm biopython*.tar.gz biopython.txt
+				#	Check for Pip
+				#	Install BioPython using Pip
+			getPip
+            pip install biopython
 			;;
 		* )
 			echo "Nothing missing"
