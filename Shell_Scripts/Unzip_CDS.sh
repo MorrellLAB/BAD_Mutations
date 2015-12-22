e24ea8e02f6d599a61e72f57638ce792a4dc34e6
TomJKono <kono.yoshitaka@gmail.com>
2015-12-21 18:07:56 -0600

    Fix empty FASTA record problem

--- Shell_Scripts/Unzip_CDS.sh
+++ Shell_Scripts/Unzip_CDS.sh
@@ -18,7 +18,7 @@ YMD=$(date +%Y%m%d)
 #   Build a new filename, we replace the .gz with nothing
 NEW_FILE=${CDS/.gz/}
 #   Ungzip the files, and drop it into the same directory
-gzip -cd $CDS > ${NEW_FILE}
+gzip -cd ${CDS} > ${NEW_FILE}
 #   Create a log file
 LOG=${NEW_FILE}.${YMD}_makeblastdb_log
 #   And an error file
@@ -28,4 +28,10 @@ ERR=${NEW_FILE}.${YMD}_makeblastdb_err
 #   would also like them to be saved on disk for reference.
 echo "Log file is stored in $LOG"
 echo "Error file is stored in $ERR"
+#	We have to remove empty sequences from the databases, as they cause FASTA
+#	parse errors. Set the record separator to > and the field separator to
+#	newline. If the second field exists, print it out, otherwise, skip it.
+awk 'BEGIN { RS=">"; FS="\n"; ORS="" } $2 { print ">"$0}' ${NEW_FILE} > \
+	tmp.fasta && \
+	mv tmp.fasta ${NEW_FILE}
 ${MAKE_BLAST_DB} -in ${NEW_FILE} -dbtype nucl > >(tee $LOG) 2> >(tee $ERR >&2)
