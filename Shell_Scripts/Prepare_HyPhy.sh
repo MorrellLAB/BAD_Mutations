10890125bf5c8df5d857b305b0963b2359bc994a
Tom Kono <konox006@umn.edu>
2017-01-09 11:16:36 -0600

    Fix sed options in HyPhy preparation script

--- Shell_Scripts/Prepare_HyPhy.sh
+++ Shell_Scripts/Prepare_HyPhy.sh
@@ -22,7 +22,7 @@ if [ "$(uname)" == "Linux" ]
         sed -i -r "s/\+//g" ${TREE}
         sed -i -r "s/\./_/g" ${TREE}
         sed -i -r "s/\./_/g" ${FASTA}
-        sed -i -r "s/:\([0-9]\)_/:\1\./g" ${TREE}
+        sed -i -e "s/:\([0-9]\)_/:\1\./g" ${TREE}
 #   The equivalent is -E in MacOS
 elif [ "$(uname)" == "Darwin" ]
     then
