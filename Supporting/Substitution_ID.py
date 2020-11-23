76d49365b60958a0ae2d220fc5d067a360746004
Samuel Hamann <haman152@umn.edu>
2020-11-23 12:42:38 -0600

    Changed file output to print to stdout, and final 'Finished ...'  message should use stderr instead

--- Supporting/Substitution_ID.py
+++ Supporting/Substitution_ID.py
@@ -8,17 +8,17 @@ are taken in this order as arguments:
 Code by Sam Hamman & Peter Morrell, borrowing heavily from Tom Kono code here: \
 https://github.com/MorrellLAB/BAD_Mutations/blob/dev/Supporting/VeP_to_Subs.py
 """
-​
+
 import sys
 import os
-​
+
 try:
     combined = sys.argv[1]
     subs_file = sys.argv[2]
 except IndexError:
     sys.stderr.write(__doc__)
     exit(1)
-​
+
 try:
     fp = os.path.abspath(os.path.expanduser(combined))
     h = open(combined, 'r')
@@ -27,7 +27,7 @@ except (OSError, PermissionError) as e:
     sys.stderr.write('Error: The combined report text file could not be read. Either the file \
         does not exist or the permissions are not open.\n')
     exit(4)
-​
+
 try:
     fp = os.path.abspath(os.path.expanduser(subs_file))
     h = open(subs_file, 'r')
@@ -36,7 +36,7 @@ except (OSError, PermissionError) as e:
     sys.stderr.write('Error: The "long substitutions" text file could not be read. Either the file \
     does not exist or the permissions are not open.\n')
     exit(4)
-​
+
 # Start writing the files. We will store the data in a dictionary so that we
 # can make sure that the substitutions are grouped by transcript before writing
 # to disk.
@@ -58,18 +58,18 @@ with open(subs_file, 'rt') as f:
             # Need to remove nucleotide information in the position
             # pos = tmp[3].replace([-4:], '')
             pos = tmp[3][:-4]
-​
+
             key = (txid,aa_pos)
             subs.update({key:pos})
-​
+
 # Begin empty dict for final output
 # Keys are pairs of gene id and position on said gene
 # Values are the data values from the combined report
 report = {}
-​
+
 # Grab header line from combined report
 header_line = ""
-​
+
 with open(combined, 'rt') as f:
     for line in f:
         if line.startswith('#'):
@@ -81,21 +81,20 @@ with open(combined, 'rt') as f:
             # We need every line of the combined report
             txid = tmp[1]        # transcript/gene id
             aa_pos = tmp[2]      # cds position
-​
+
             # Grabs the substitution for the current position
             key = (txid, aa_pos)
             tmp[0] = subs[key]
-​
+
             # Adds the entire adjusted row of data under
             # the unique key pair to the final dicitonary
             report.update({key:tmp})
-​
+
 # Write to a temporary text file, grouped first by gene id then by position
 # Also note, sorted lexicographically, not numerically (e.g. 123, 32, 89, etc)
-with open('tmp.txt', 'wt') as f_out:
-    f_out.write(header_line)
-    for key in sorted(report.keys()):
-        out_string = '\t'.join(report[key]) + '\n'
-        f_out.write(out_string)
-​
-print('Finished converting')
+print(header_line[:-1])  # Remove newline character to avoid double-newlining
+for key in sorted(report.keys()):
+    out_string = '\t'.join(report[key])
+    print(out_string)
+
+sys.stderr.write('Finished converting.')
